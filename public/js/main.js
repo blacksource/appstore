452011cfef33280dc1f72be04f56fe4c9e832ff9
blacksource <blacksource.hw@gmail.com>
2012-07-11 22:53:08 +0800

    bug:app show style

--- public/js/main.js
+++ public/js/main.js
@@ -61,19 +61,19 @@ $(function(){
     });
     
 	$(".yui3-imagelist-arrow-left").click(function(){
-            var width=206;
-            var left = parseInt($("#shot-list").css("left").replace("px", ""));
-            var index = left/width;
-            if(index < 0)
-            {
-                left = left + width;
-                $("#shot-list").animate({left: left + "px"});
-                $(".yui3-imagelist-arrow-right").removeClass("yui3-imagelist-arrow-right-disabled");    
-            }
-            else
-            {
-                $(".yui3-imagelist-arrow-left").addClass("yui3-imagelist-arrow-left-disabled");
-            }
+        var width=206;
+        var left = parseInt($("#shot-list").css("left").replace("px", ""));
+        var index = left/width;
+        if(index < 0)
+        {
+            left = left + width;
+            $("#shot-list").animate({left: left + "px"});
+            $(".yui3-imagelist-arrow-right").removeClass("yui3-imagelist-arrow-right-disabled");    
+        }
+        else
+        {
+            $(".yui3-imagelist-arrow-left").addClass("yui3-imagelist-arrow-left-disabled");
+        }
     });
     $(".yui3-imagelist-arrow-right").click(function(){
         var width=206;
